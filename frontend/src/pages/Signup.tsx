1e5f34ba116e302cfe0340061ca26e1d70934d2e
leogo22mz <leogo2206@gmail.com>
2024-02-22 16:29:14 +0000

    FRONT: Implemented IMG and Username to SignUp

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Form, Input, Button, message, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button, message, Checkbox, Upload, Avatar } from 'antd';
+import { UserOutlined, LockOutlined, UploadOutlined } from '@ant-design/icons';
 import axios from 'axios';
 import './Survey.css';
+import { UploadChangeParam } from 'antd/lib/upload';
 
 function Register() {
   const navigate = useNavigate();
@@ -12,16 +13,21 @@ function Register() {
   const [isRegistered, setIsRegistered] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const onFinish = async (values: any) => {
+  // Image upload state
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const onFinish = async (values: { email: any; password: any; username: any; }) => {
     setLoading(true);
     try {
       const response = await axios.post('http://localhost:3000/signup', {
         user: {
           email: values.email,
-          password: values.password
+          password: values.password,
+          username: values.username,
+          profileImage: imageUrl // Send the profile image URL to the backend
         }
       });
-  
+
       if (response.status === 200) {
         localStorage.setItem('token', response.data.token);
         message.success('Registro exitoso!');
@@ -36,7 +42,6 @@ function Register() {
     }
     setLoading(false);
   };
-  
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -50,13 +55,20 @@ function Register() {
     window.location.reload();
   };
 
+const handleImageUpload = (info: UploadChangeParam<any>) => {
+  if (info.file.status === 'done' && info.file.response) {
+    setImageUrl(info.file.response.imageUrl);
+  }
+};
+
+
   return (
     <div className={`register-container ${isMenuOpen ? 'menu-open' : ''}`}>
       <div className="topbar">
         <button className="menu-toggle" onClick={toggleMenu}>☰</button>
       </div>
       <div className="sidebar">
-      <ul className="sidebar-menu">
+        <ul className="sidebar-menu">
           <br /><br />
           <li className="menu-item" onClick={() => navigate('/')}>Home</li>
           <li className="menu-item" onClick={() => navigate('/')}>My Surveys</li>
@@ -68,39 +80,87 @@ function Register() {
       <div className="login-container">
         <h1>Sign Up</h1><br />
         <Form
-        name="register"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          name="email"
-          rules={[
-            { required: true, message: 'Please, introduce your email!' },
-            { type: 'email', message: '¡E-mail is not valid!' }
-          ]}
+          name="register"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
         >
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="E-mail" />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'Por favor, introduce tu contraseña!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-
-
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
-            Register
-          </Button>
-          Already have an account? <a href="" onClick={() => navigate('/login')}>¡Log In now!</a>
-        </Form.Item>
-      </Form>
+          <Form.Item
+            name="email"
+            rules={[
+              { required: true, message: 'Please, introduce your email!' },
+              { type: 'email', message: '¡E-mail is not valid!' }
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="E-mail" />
+          </Form.Item>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please, introduce your username!' }]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Por favor, introduce tu contraseña!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                },
+              }),
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Confirm Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76" // Your image upload endpoint
+              onChange={handleImageUpload}
+            >
+              {imageUrl ? (
+                <Avatar size={64} src={imageUrl} alt="Avatar" />
+              ) : (
+                <div>
+                  <UploadOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </div>
+              )}
+            </Upload>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button" loading={loading}>
+              Register
+            </Button>
+            Already have an account? <a href="" onClick={() => navigate('/login')}>¡Log In now!</a>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
